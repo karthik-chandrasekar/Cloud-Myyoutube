45ad5c494536aca608adc8b00ee54860cd76cc1f
System Administrator <root@Karthiks-MacBook-Pro.local>
2013-11-17 17:04:04 -0700

    Second version of myyoutube, basic functionalities inplace

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Library/Python/2.7/site-packages/django/bin/mysite/db/myyoutube.db',                      # Or path to database file if using sqlite3.
+        'NAME': '/Library/Python/2.7/site-packages/django/bin/Raf-Changes/Cloud-Myyoutube-master/mysite/db/myyoutube.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -111,7 +111,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/Library/Python/2.7/site-packages/django/bin/mysite/static_files",
+    "/Library/Python/2.7/site-packages/django/bin/Raf-Changes/Cloud-Myyoutube-master/mysite/static_files/",
 )
 
 INSTALLED_APPS = (
--- mysite/myyoutube/views.py
+++ mysite/myyoutube/views.py
@@ -9,60 +9,85 @@ from django.template import RequestContext
 from datetime import datetime
 from models import AllVidFileUrl
 
+
 class UploadForm(forms.Form):
     file = forms.FileField(label='Select photo to upload')
 
 
 class RateForm(forms.Form):
-     rate = forms.CharField()
+    rate = forms.CharField()
 
 def index(request):
-    def store_in_s3(filename, content, b):
-        mime = mimetypes.guess_type(filename)[0]
+    def store_in_s3(p_obj, content, b):
+        file_name = p_obj.name
+        mime = mimetypes.guess_type(file_name)[0]
         k = Key(b)
-        k.key = filename
+        #import pdb;pdb.set_trace()
+        
+        k.key = p_obj.id
         k.set_metadata("Content-Type", mime)
         k.set_contents_from_string(content)
         k.set_acl("public-read")
       
-    def display(b):
-        all_files_url_list = []
+    
+
+    def delete(file_id, b):
         k = Key(b)
-        for ele in k.bucket.get_all_keys():
-            file_name = ele.name
-            file_url = "http://s3.amazonaws.com/rafmyyoutubebucket/"+file_name
-            all_files_url_list.append(file_url)
-        return all_files_url_list
+        k.key = file_id
+        b.delete_key(k)
+        
+            
+        
         
 
     #Creating S3 connection 
     conn = S3Connection(settings.AWS_ACCESS_KEY, settings.AWS_SECRET_KEY)
     b = conn.create_bucket("rafmyyoutubebucket")
-    all_files_url_list = display(b)
 
-    photos = AllVidFileUrl.objects.all()#.order_by("uploaded")
+    
+
+
+    photos = AllVidFileUrl.objects.all().order_by("-rate")
     if not request.method == "POST":
         f = UploadForm()
-        return render_to_response("myyoutube/index.html", {"form":f, "photos":photos, "files_url_list":all_files_url_list}, context_instance=RequestContext(request))
+        return render_to_response("myyoutube/index.html", {"form":f, "photos":photos}, context_instance=RequestContext(request))
     
     f = UploadForm(request.POST, request.FILES)
-    if ((not f.is_valid()) and (request.POST.get('rate', 0) == 0)):
-        return render_to_response("myyoutube/index.html", {"form":f, "photos":photos, "files_url_list":all_files_url_list}, context_instance=RequestContext(request))
+    
     if f.is_valid():     
-        file = request.FILES["file"]
-        filename = file.name
-        content = file.read()
-        store_in_s3(filename, content, b)
-        p_obj = AllVidFileUrl(url="http://s3.amazonaws.com/rafmyyoutubebucket/" + filename, uploaded=datetime.now(), name=filename, rate=0, rate_num=0)
+        fil = request.FILES["file"]
+        file_name = fil.name
+        p_obj = AllVidFileUrl(url="http://s3.amazonaws.com/rafmyyoutubebucket/", uploaded=datetime.now(), name=file_name, rate=0, rate_num=0)
         p_obj.save()
+        content = fil.read()
+        store_in_s3(p_obj, content, b)
    
     elif (request.POST.get('rate', 0) !=0):
-        r = request.POST.get('rate', 0)
-        photos = AllVidFileUrl.objects.all()#.order_by("-uploaded")
-        for file_obj in photos:
-            file_obj.rate_num += 1
-            file_obj.rate = (file_obj.rate + int(r)) / file_obj.rate_num
-            file_obj.save() 
-        photos = AllVidFileUrl.objects.all()#.order_by("-uploaded")
+        r_temp = request.POST.get('rate', 0)
         
-    return render_to_response("myyoutube/index.html", {"form":f, "photos":photos, "files_url_list":all_files_url_list}, context_instance=RequestContext(request))
+        
+        r = r_temp[0]
+        file_id = r_temp[1:]
+        for file_obj in AllVidFileUrl.objects.all():
+            if file_obj.id==int(file_id):
+                file_obj.rate_num += 1
+                file_obj.rate = (file_obj.rate + int(r))/ file_obj.rate_num
+                file_obj.save()
+       
+    elif request.POST.get('delete'):
+        file_id = request.POST.get('delete')
+        delete(file_id, b)
+        
+        for file_obj in AllVidFileUrl.objects.all():
+            if file_obj.id==int(file_id):
+                file_obj.delete()
+                
+
+        
+            
+    else:
+        return render_to_response("myyoutube/index.html", {"form":f, "photos":photos}, context_instance=RequestContext(request))    
+
+    
+    photos = AllVidFileUrl.objects.all().order_by("-rate")
+    return render_to_response("myyoutube/index.html", {"form":f, "photos":photos}, context_instance=RequestContext(request))
